f6a276f46640c8e7c88d76097644ccc90bb617de
Matsko Egor <76002087+fuett966@users.noreply.github.com>
2021-09-03 12:59:05 +0300

    Test 2
    add: nothing

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -18,32 +18,39 @@ public class Character : MonoBehaviour
     [Header("Actual state")]
     public State CurrentState;
 
-    float eatEndTime = 15f;
-    float energyEndTime = 25f;
+    float eatEndTime = 25f;
+    float energyEndTime = 85f;
 
-    void Start(){
+    void Start()
+    {
         SetState(StartState);
     }
 
-    void Update(){
+    void Update()
+    {
         Eat -= Time.deltaTime/eatEndTime;
         Energy -= Time.deltaTime / energyEndTime;
 
-        if (!CurrentState.IsFinished){
+        if (!CurrentState.IsFinished)
+        {
             CurrentState.Run();
         }
-        else{
-            if (Eat <= 0.5f){SetState(EatState);}
+        else
+        {
+            if (Eat <= -1){Destroy(this.gameObject);}
+            else if (Eat <= 0.5f){SetState(EatState);}
             else if (Energy <= 0.4f){SetState(EnergyState);}
             else{SetState(RandomMoveState);}
         }
     }
-    public void SetState (State state){
+    public void SetState (State state)
+    {
         CurrentState = Instantiate(state);
         CurrentState.Character = this;
         CurrentState.Init();
     }
-    public void MoveTo(Vector3 position){
+    public void MoveTo(Vector3 position)
+    {
         position.y = transform.position.y;
 
         transform.position = Vector3.MoveTowards(current: transform.position ,target: position ,Time.deltaTime);
---
+++ Assets/Scripts/DeadState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class DeadState : State
+{
+    public override void Init()
+    {
+        if (Character.Eat >= -1)
+        {
+            return;
+        }
+        else
+        {
+            Die();
+        }
+    }
+    public override void Run ()
+    {
+        Die();
+    }
+    public void Die()
+    {
+        Destroy(Character.GetComponent<Character>().gameObject);
+    }
+}
